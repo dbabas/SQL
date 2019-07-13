a68be8968aee5c0fdfa6f8edf86fc57687390eba
Dimitrios Bampas <dimitrios.bampas@thenavpeople.co.uk>
2019-07-13 15:44:32 +0300

    login to any NAV database

---
+++ NAV/Login to a copy of a NAV database (2013 onward).sql
@@ -0,0 +1,13 @@
+delete from [dbo].[User]
+
+delete from [dbo].[Access Control]
+
+delete from [dbo].[User Property]
+
+    delete from [dbo].[Page Data Personalization]
+
+delete from [dbo].[User Default Style Sheet]
+
+delete from [dbo].[User Metadata]
+
+delete from [dbo].[User Personalization]
