9f26728b96cce5b91dda50e1a296eabd13d10f48
Dimitrios Bampas <dimitrios.bampas@thenavpeople.co.uk>
2020-03-27 09:56:57 +0200

    Updated scripts

---
+++ Maintenance Tasks/Set MaxDOP back to 1.sql
@@ -0,0 +1,10 @@
+USE master;  
+GO   
+EXEC sp_configure 'show advanced options', 1;  
+GO  
+RECONFIGURE WITH OVERRIDE;  
+GO  
+EXEC sp_configure 'max degree of parallelism', 1;  
+GO  
+RECONFIGURE WITH OVERRIDE;  
+GO 
