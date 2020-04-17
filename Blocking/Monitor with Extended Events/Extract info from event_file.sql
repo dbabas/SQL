7fd62a357ec34a49a0fe7f8d4d4007341d78d3ab
Dimitrios Bampas <dimitrios.bampas@thenavpeople.co.uk>
2020-04-18 01:06:28 +0300

    New scripts

---
+++ Blocking/Monitor with Extended Events/Extract info from event_file.sql
@@ -0,0 +1,47 @@
+﻿--Extract info about blocking from ring_buffer
+
+DECLARE @ExtendedEventsSessionName sysname = N'Blocking Monitoring'; --enter here the name of Extended Event session
+DECLARE @StartTime datetimeoffset;
+DECLARE @EndTime datetimeoffset;
+DECLARE @Offset int;
+DECLARE @path NVARCHAR(260) = N'C:\Temp\Blocks*'; -- Enter here the path and filename. Keep the asterisk to query all files.
+SET @StartTime = DATEADD(HOUR, -4, GETDATE()); --modify this to suit your needs
+SET @EndTime = GETDATE();
+SET @Offset = DATEDIFF(MINUTE, GETDATE(), GETUTCDATE());
+SET @StartTime = DATEADD(MINUTE, @Offset, @StartTime);
+SET @EndTime = DATEADD(MINUTE, @Offset, @EndTime);
+
+DROP TABLE IF EXISTS #xmlResults;
+CREATE TABLE #xmlResults
+(
+      xeTimeStamp datetimeoffset NOT NULL
+    , xeXML XML NOT NULL
+	, xeBlockDuration bigint
+);
+ 
+ 
+SELECT StartTimeUTC = CONVERT(varchar(30), @StartTime, 127)
+    , StartTimeLocal = CONVERT(varchar(30), DATEADD(MINUTE, 0 - @Offset, @StartTime), 120)
+    , EndTimeUTC = CONVERT(varchar(30), @EndTime, 127)
+    , EndTimeLocal = CONVERT(varchar(30), DATEADD(MINUTE, 0 - @Offset, @EndTime), 120);
+ 
+
+WITH src
+AS (SELECT CAST(fx.event_data AS XML) AS Event_Data
+    FROM sys.fn_xe_file_target_read_file(@path,
+                                         NULL,
+                                         NULL,
+                                         NULL) AS fx )
+ 
+INSERT INTO #xmlResults (xeXML, xeTimeStamp, xeBlockDuration)
+SELECT src.Event_Data
+    , [xeTimeStamp] = src.Event_Data.value('(/event/@timestamp)[1]', 'datetimeoffset(7)')
+	, [xeBlockDuration] = src.Event_Data.value('(event/data/value/blocked-process-report/blocked-process/process/@waittime)[1]', 'bigint')
+FROM src;
+ 
+SELECT [TimeStamp] = CONVERT(varchar(30), DATEADD(MINUTE, 0 - @Offset, xr.xeTimeStamp), 120)
+    , xr.xeXML, xr.xeBlockDuration
+FROM #xmlResults xr
+WHERE xr.xeTimeStamp >= @StartTime
+    AND xr.xeTimeStamp<= @EndTime
+ORDER BY xr.xeTimeStamp;
---
+++ Blocking/Monitor with Extended Events/Extract info from ring_buffer.sql
@@ -0,0 +1,52 @@
+﻿--Extract info about blocking from ring_buffer
+
+DECLARE @ExtendedEventsSessionName sysname = N'Blocking Monitoring'; --enter here the name of Extended Event session
+DECLARE @StartTime datetimeoffset;
+DECLARE @EndTime datetimeoffset;
+DECLARE @Offset int;
+ 
+DROP TABLE IF EXISTS #xmlResults;
+CREATE TABLE #xmlResults
+(
+      xeTimeStamp datetimeoffset NOT NULL
+    , xeXML XML NOT NULL
+    , xeBlockDuration bigint
+);
+ 
+SET @StartTime = DATEADD(HOUR, -4, GETDATE()); --modify this to suit your needs
+SET @EndTime = GETDATE();
+SET @Offset = DATEDIFF(MINUTE, GETDATE(), GETUTCDATE());
+SET @StartTime = DATEADD(MINUTE, @Offset, @StartTime);
+SET @EndTime = DATEADD(MINUTE, @Offset, @EndTime);
+ 
+SELECT StartTimeUTC = CONVERT(varchar(30), @StartTime, 127)
+    , StartTimeLocal = CONVERT(varchar(30), DATEADD(MINUTE, 0 - @Offset, @StartTime), 120)
+    , EndTimeUTC = CONVERT(varchar(30), @EndTime, 127)
+    , EndTimeLocal = CONVERT(varchar(30), DATEADD(MINUTE, 0 - @Offset, @EndTime), 120);
+ 
+DECLARE @target_data xml;
+SELECT @target_data = CONVERT(xml, target_data)
+FROM sys.dm_xe_sessions AS s 
+JOIN sys.dm_xe_session_targets AS t 
+    ON t.event_session_address = s.address
+WHERE s.name = @ExtendedEventsSessionName
+    AND t.target_name = N'ring_buffer';
+ 
+;WITH src AS 
+(
+    SELECT xeXML = xm.s.query('.')
+    FROM @target_data.nodes('/RingBufferTarget/event') AS xm(s)
+)
+INSERT INTO #xmlResults (xeXML, xeTimeStamp, xeBlockDuration)
+SELECT src.xeXML
+    , [xeTimeStamp] = src.xeXML.value('(/event/@timestamp)[1]', 'datetimeoffset(7)')
+    , [xeBlockDuration] = src.xeXML.value('(event/data/value/blocked-process-report/blocked-process/process/@waittime)[1]', 'bigint')
+FROM src;
+ 
+SELECT [TimeStamp] = CONVERT(varchar(30), DATEADD(MINUTE, 0 - @Offset, xr.xeTimeStamp), 120)
+    , xr.xeXML
+	, xr.xeBlockDuration
+FROM #xmlResults xr
+WHERE xr.xeTimeStamp >= @StartTime
+    AND xr.xeTimeStamp<= @EndTime
+ORDER BY xr.xeTimeStamp;
